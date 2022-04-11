44a96567b2584372b10623af8d1ffeb80bb985c9
Hugh Saalmans <hugh.saalmans@iag.com.au>
2022-04-11 10:03:56 +1000

    fixed GNAFPIDs missing LGA PIDs

--- postgres-scripts/xx-04-02-manual-bdy-tags.sql
+++ postgres-scripts/xx-04-02-manual-bdy-tags.sql
@@ -1,28 +1,54 @@
 
 
+-- manual fixes for addresses with no LGA (these are mostly valid nulls. e.g. ACT has no councils)
+-- 149 addresses (as at 202202 will not be fixed -- these are all offshore points, a number being oyster leases and boat moorings
 
-
-
--- fix 35 boatsheds
+-- all of ACT -- 232,665 rows
 update gnaf_202202.address_principal_admin_boundaries
-    set lga_pid = 'lgacbffb11990f2',
-        lga_name = 'Hobart City'
+set lga_pid = 'lgaact9999991',
+    lga_name = 'Unincorporated ACT'
+where state = 'ACT'
+;
+
+-- Specific localities
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lgaot9999991',
+    lga_name = 'Unincorporated OT (Norfolk Island)'
+where locality_pid = 'locc15e0d2d6f2a'
+  and lga_pid is null;
+
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lgaot9999992',
+    lga_name = 'Unincorporated OT (Jervis Bay)'
+where locality_pid = 'loced195c315de9'
+  and lga_pid is null;
+
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lgasa9999991',
+    lga_name = 'Unincorporated SA (Thistle Island)'
+where locality_pid = '250190776'
+  and lga_pid is null;
+
+-- 35 boatsheds in Hobart
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lgacbffb11990f2',
+    lga_name = 'Hobart City'
 where locality_pid = 'loc0f7a581b85b7'
-    and lga_pid is null;
+  and lga_pid is null;
 
--- Missing LGAs - not fixed -- no LGA, oyster lease etc..., or small number of addresses
--- +-------------+---------------+--------------+--------+-----+
--- |address_count|locality_pid   |locality_name |postcode|state|
--- +-------------+---------------+--------------+--------+-----+
--- All of ACT
--- |2150         |locc15e0d2d6f2a|NORFOLK ISLAND|2899    |OT   |
--- |180          |loced195c315de9|JERVIS BAY    |2540    |OT   |
--- |51           |250190776      |THISTLE ISLAND|5606    |SA   |
--- |23           |loccf8be9dcdacd|SMOKY BAY     |5680    |SA   |
--- |8            |loc552bd3aef1b8|JENNINGS      |4383    |NSW  |
--- +-------------+---------------+--------------+--------+-----+
+-- slightly offshore points in SA
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lgaa8d127fa14e7',
+    lga_name = 'Ceduna'
+where locality_pid = 'loccf8be9dcdacd'
+  and lga_pid is null;
+
+-- NSW/QLD border silliness
+update gnaf_202202.address_principal_admin_boundaries
+set lga_pid = 'lga7872e04f6637',
+    lga_name = 'Tenterfield'
+where locality_pid = 'loc552bd3aef1b8'
+  and lga_pid is null;
 
 
--- 106 localities with =< 4 addresses (mostly coastal, some mid-river)...
-
-
+select * from gnaf_202202.address_principal_admin_boundaries;
